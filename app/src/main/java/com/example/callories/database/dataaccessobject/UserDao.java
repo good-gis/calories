837c560e2b2473f7a7e48b94f5d4201fe4933719
Ivan Glushkov <glushkov1997@icloud.com>
2022-01-29 23:43:30 +0300

    added main activity

--- app/src/main/java/com/example/callories/database/dataaccessobject/UserDao.java
+++ app/src/main/java/com/example/callories/database/dataaccessobject/UserDao.java
@@ -17,9 +17,8 @@ public interface UserDao {
     @Query("SELECT * FROM user WHERE uid IN (:userIds)")
     List<User> loadAllByIds(int[] userIds);
 
-    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-            "last_name LIKE :last LIMIT 1")
-    User findByName(String first, String last);
+    @Query("SELECT * FROM user WHERE phone LIKE :phone LIMIT 1")
+    User findByPhone(String phone);
 
     @Insert
     void insertAll(User... users);
--- app/src/main/java/com/example/callories/database/entity/User.java
+++ app/src/main/java/com/example/callories/database/entity/User.java
@@ -9,9 +9,15 @@ public class User {
     @PrimaryKey
     public int uid;
 
-    @ColumnInfo(name = "first_name")
-    public String firstName;
+    @ColumnInfo(name = "name")
+    public String name;
 
-    @ColumnInfo(name = "last_name")
-    public String lastName;
+    @ColumnInfo(name = "phone")
+    public String phone;
+
+    @ColumnInfo(name = "password")
+    public String password;
+
+    @ColumnInfo(name = "is_remember_me")
+    public Boolean isRememberMe;
 }
