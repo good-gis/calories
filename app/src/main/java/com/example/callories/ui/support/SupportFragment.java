944d030d4caf6f2267fa49341bcfe9551fce8459
Ivan Glushkov <glushkov1997@icloud.com>
2022-02-12 19:32:40 +0300

    support screen is ready

--- app/src/main/java/com/example/callories/ui/support/SupportFragment.java
+++ app/src/main/java/com/example/callories/ui/support/SupportFragment.java
@@ -1,55 +1,45 @@
 package com.example.callories.ui.support;
 
-import androidx.lifecycle.ViewModelProvider;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.example.callories.R;
-import com.example.callories.databinding.FragmentSlideshowBinding;
 import com.example.callories.databinding.FragmentSupportBinding;
+import com.example.callories.helpers.NotifyHelper;
 
 public class SupportFragment extends Fragment {
-
-    private SupportViewModel mViewModel;
     private FragmentSupportBinding binding;
-//
-//    public static SupportFragment newInstance() {
-//        return new SupportFragment();
-//    }
-//
-//    @Override
-//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-//                             @Nullable Bundle savedInstanceState) {
-//        return inflater.inflate(R.layout.fragment_support, container, false);
-//    }
-//
-//    @Override
-//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-//        super.onActivityCreated(savedInstanceState);
-//        mViewModel = new ViewModelProvider(this).get(SupportViewModel.class);
-//        // TODO: Use the ViewModel
-//    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-       SupportViewModel supportViewModel =
-                new ViewModelProvider(this).get(SupportViewModel.class);
 
         binding = FragmentSupportBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSupport;
-        supportViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        binding.buttonSentMail.setOnClickListener(v -> {
+            if (binding.textOfMail.getText().toString().isEmpty() || binding.textOfMail.getText().toString().isEmpty()) {
+                NotifyHelper.showFastToast(getContext(), R.string.email_support_validation);
+            } else {
+                composeEmail(new String[]{getString(R.string.my_email)}, binding.themeOfMail.getText().toString(), binding.textOfMail.getText().toString());
+            }
+        });
+
+        binding.vk.setOnClickListener(v -> {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.vk_link))));
+        });
+
+        binding.telegram.setOnClickListener(v -> {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.telegram_link))));
+        });
+
         return root;
     }
 
@@ -59,4 +49,14 @@ public class SupportFragment extends Fragment {
         binding = null;
     }
 
+    public void composeEmail(String[] addresses, String subject, String textOfEmail) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, textOfEmail);
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 }
